af6e7ef85511d01e35f82a6c671c28c63a8fffb9
Elżbieta Neska <ela.neska@wp.pl>
2019-01-06 22:30:09 +0100

    add js

---
+++ src/js/app_article.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    //hamburger
+    let hamburger = document.querySelector('#hamburger');
+    let menu = document.querySelector('#menu');
+    let wrap = document.querySelector('#wrap');
+    let back = document.querySelector('#back');
+
+    const resizeMenu = () => {
+        if (menu.classList.contains("open_menu")) {
+            wrap.classList.remove("open_menu_wrap");
+            menu.setAttribute("class", "menu");
+        }
+    };
+
+    hamburger.onclick = () => {
+        menu.setAttribute("class", "open_menu");
+        wrap.classList.add("open_menu_wrap");
+    };
+
+    back.onclick = () => {
+        wrap.classList.remove("open_menu_wrap");
+        menu.setAttribute("class", "menu");
+    };
+
+    window.addEventListener("resize", resizeMenu);
+
+    //border menu
+    let borderMenu = () => {
+        let elMenu = document.querySelector("nav").getElementsByTagName('a');
+
+        for (let i = 0; i < elMenu.length; i++) {
+            if (document.location.href.indexOf(elMenu[i].href) >= 0) {
+                elMenu[i].className = 'active';
+            }
+        }
+    };
+    borderMenu();
+
+    //button more
+    let btnMore = document.querySelector("#btnMore");
+    const realization = Array(...document.querySelectorAll(".realization"));
+    let w = window.innerWidth;
+    let n;
+    let k;
+
+    if (w <= 1024) {
+        n = 3;
+    } else {
+        n = 4;
+    }
+
+    if (w <= 1024) {
+        k = 3;
+    } else {
+        k = 4;
+    }
+    let changeDisplay = (elem, property) => {
+        elem.forEach(el => el.style.display = property);
+    };
+
+    let row = realization.slice(0, n);
+    changeDisplay(row, "grid");
+    btnMore.addEventListener("click", function () {
+
+        for (let i = 0; i < 1; i++) {
+            n = n + k;
+            let rowGrid = realization.slice(0, n);
+            changeDisplay(rowGrid, "grid");
+            realization[n - k].scrollIntoView();
+            if (realization.length <= n) {
+                btnMore.style.display = "none";
+            }
+        }
+    });
+});
---
+++ src/js/app_home.js
@@ -0,0 +1,112 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    let hamburger = document.querySelector('#hamburger');
+    let menu = document.querySelector('#menu');
+    let wrap = document.querySelector('#wrap');
+    let back = document.querySelector('#back');
+
+    const resizeMenu = () => {
+        if (menu.classList.contains("open_menu")) {
+            wrap.classList.remove("open_menu_wrap");
+            menu.setAttribute("class", "menu");
+        }
+    };
+
+    hamburger.onclick = () => {
+        menu.setAttribute("class", "open_menu");
+        wrap.classList.add("open_menu_wrap");
+    };
+
+    back.onclick = () => {
+        wrap.classList.remove("open_menu_wrap");
+        menu.setAttribute("class", "menu");
+    };
+
+    window.addEventListener("resize", resizeMenu);
+
+    //slider
+    let slideIndex = 0;
+    let bgArray = ["image/slider1.png", "image/slider2.png"];
+    let slider = document.querySelector(".slider");
+    let leftSlider = document.querySelector("#left");
+    let rightSlider = document.querySelector("#right");
+
+    let changeBgSlider = () => {
+        slider.style.backgroundImage = "url('" + bgArray[slideIndex] + "')";
+    };
+    let nextSlide = () => {
+        if (slideIndex < bgArray.length - 1) {
+            slideIndex += 1;
+        } else if (slideIndex === bgArray.length - 1) {
+            slideIndex = 0;
+        }
+        changeBgSlider();
+    };
+    let prevSlide = () => {
+        if (slideIndex === 0) {
+            slideIndex = bgArray.length - 1;
+        } else if (slideIndex <= bgArray.length - 1) {
+            slideIndex -= 1;
+        }
+        changeBgSlider();
+    };
+
+    rightSlider.addEventListener("click", () => {
+        nextSlide()
+    });
+    leftSlider.addEventListener("click", () => {
+        prevSlide()
+    });
+
+    //border menu
+    let borderMenu = () => {
+        let elMenu = document.querySelector("nav").getElementsByTagName('a');
+
+        for (let i = 0; i < elMenu.length; i++) {
+            if (document.location.href.indexOf(elMenu[i].href) >= 0) {
+                elMenu[i].className = 'active';
+            }
+        }
+    };
+    borderMenu();
+
+    //slider realization
+    let leftRealization = document.querySelector("#left__realization");
+    let rightRealization = document.querySelector("#right__realization");
+    const realization = Array(...document.querySelectorAll(".realization"));
+    let n = 3;
+    let row = realization.slice(0, n);
+    row.forEach(el => el.style.display = "grid");
+
+    let changeDisplay = (elem, property)=>{
+        elem.forEach(el => el.style.display = property);
+    };
+
+    rightRealization.addEventListener("click", () => {
+
+        if (n < realization.length) {
+
+            for (let i = 0; i < 1; i++) {
+                let rowNone = realization.slice(n - 3, n);
+                changeDisplay(rowNone, "none");
+                let rowGrid = realization.slice(n, n + 3);
+                changeDisplay(rowGrid, "grid")
+            }
+            n += 3;
+        }
+    });
+
+    leftRealization.addEventListener("click", () => {
+
+        if (n > 3) {
+            for (let i = 0; i < 1; i++) {
+                let rowNone = realization.slice(n - 3, n);
+                changeDisplay(rowNone, "none");
+                n -= 3;
+                let rowGrid = realization.slice(n - 3, n);
+                changeDisplay(rowGrid, "grid")
+            }
+        }
+    });
+});
